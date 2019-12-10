ce3a5f76ad5aaa1d36a6a1d95871a6f6207b5806
seladanti <kwameasiago@gmail.com>
2019-12-10 16:39:05 +0300

    set up react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <React.Fragment>
+      <h1>Hello world</h1>
+    </React.Fragment>
+  );
+}
+
+export default App;
